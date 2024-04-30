86e50f82efc666c926b11edb48c58341090af5e0
ZaikaDaria <corplinebusiness@gmail.com>
2024-04-30 23:38:30 +0300

    add link to the performance

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import requests
-from bs4 import BeautifulSoup
-from flask import Flask
+import telebot
+import os
 
-app = Flask(__name__)
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_URL = "https://ft.org.ua/ua/performance/tartyuf"
-PORT = 5100
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 
+@dataclass
 class Tortuf:
-    def __init__(self, performance_date):
-        self.performance_date = performance_date
-
-    def __repr__(self):
-        return f"Tortuf(performance_date={self.performance_date})"
+    performance_date: str
+    link: str
 
 
 def get_website_data():
@@ -21,24 +25,30 @@ def get_website_data():
     response = requests.get(BASE_URL)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
-        for item in soup.select(".performanceevents_item_info_date"):
-            performance_dates = item.get_text(strip=True).split()[1]
-            performance_list.append(Tortuf(performance_date=performance_dates))
+        for item in soup.select(".performanceevents_item"):
+            performance_dates = item.select_one(".performanceevents_item_info_date").get_text(strip=True).split()[1]
+            link_element = item.select_one(".button.hvr-shutter-out-horizontal")
+            if link_element and link_element.has_attr("onclick"):
+                link = link_element["onclick"].split("'")[1]
+            else:
+                link = None
+            performance_list.append(Tortuf(performance_date=performance_dates, link=link))
         return performance_list
 
 
-@app.route("/list")
-def list_performances():
+@bot.message_handler(commands=["list", ])
+def send_message_to_telegram(message):
     performances = get_website_data()
-    return f"Тортюф: список всіх показів: {performances}"
+    bot.reply_to(message, f"Тортюф: список всіх показів: \n{performances}")
 
 
-@app.route("/last")
-def last_performance():
+@bot.message_handler(commands=["last", ])
+def send_message_to_telegram(message):
     performances = get_website_data()
     perf_date = performances[-1].performance_date
-    return f"Тортюф: дата останнього показу: {str(perf_date)}"
+    bot.reply_to(message, f"Тортюф: дата останнього показу: {str(perf_date)}")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=PORT)
+    bot.delete_webhook()
+    bot.infinity_polling(none_stop=True)
